6d25c5c1ec7e7cc350c04631d242fc7eb8b1fda4
Addison Dalton <zarch14@gmail.com>
2019-01-23 00:03:23 -0500

    Created styled components for cart button and navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
+import styled from 'styled-components';
+import { ButtonContainer } from './Button';
 
+const NavWrapper = styled.nav `
+    background: var(--mainBlue);
+
+    .nav-link {
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capatilize;
+    }
+`
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative Commons (Attribution 3.0 Unported); https://www.iconfinder.com/Makoto_msk */}
                 <Link to='/'>
                     <img src={logo} alt="store" className="navbar-brand" />
@@ -18,12 +29,14 @@ export default class Navbar extends Component {
                     </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
-                        <i className="fas fa-cart-plus"></i>
-                        Cart
-                    </button>
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <i className="fas fa-cart-plus"></i>
+                        </span>
+                        cart
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
